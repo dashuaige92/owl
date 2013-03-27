fd4d1eaa77c2d6b619b4e94368387a0d7c9189f0
Kenneth Cheng <kenthesage@gmail.com>
2013-03-27 01:34:21 -0400

    Add minimal LexerTestCase for Owl

---
+++ test/lex/test_hello_world.py
@@ -0,0 +1,12 @@
+import unittest
+
+from test.lex_helper import LexerTestCase
+
+class TestHelloWorld(LexerTestCase):
+    def test_hello_world(self):
+        self.assertTokens('print("Hello, world!")',
+                          'print',
+                          '(',
+                          '"Hello, world!"',
+                          ')',
+                         )
---
+++ test/lex_helper.py
@@ -0,0 +1,13 @@
+import unittest
+
+from owl.lex import lexer
+
+class LexerTestCase(unittest.TestCase):
+    """A test case that includes helper assertion methods.
+    """
+    def assertTokens(self, string, *tokens, **kwargs):
+        msg = kwargs.get('msg')
+        lexer.input(string)
+        toks = tuple(map(lambda t: t.value, [tok for tok in lexer]))
+        if toks != tokens:
+            raise AssertionError(str(toks) + ' != ' + str(tokens))
