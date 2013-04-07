60bbb5d53d69ed3848109f56e60bb6e5f8acec47
unknown <blencho04@gmail.com>
2013-04-07 22:18:56 +0100

    added test_init, started test_functions

---
+++ test/lex/test_functions.py
@@ -0,0 +1,5 @@
+import unittest
+
+from test.lex_helper import LexerTestCase
+
+ 
---
+++ test/parse/test_initialization.py
@@ -0,0 +1,22 @@
+import unittest
+import textwrap
+
+from test.parse_helper import ParserTestCase
+
+class TestInitialization(ParserTestCase):
+	#def test_declare(self):
+	#	owl = r"""machine m'
+	#	"""
+	#	python = r"""
+	#	"""
+	#self.assertAST(owl, python)
+
+	def test_delare_init(self):
+		owl = textwrap.dedent(r"""
+		string s = "test"
+		""")
+		python = textwrap.dedent(r"""s = 'test'
+		""")
+		self.assertAST(owl, python)	
+	
+
