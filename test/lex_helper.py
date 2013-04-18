4d6a1674d43a5efda5dad8a42da70714362c3253
Kenneth Cheng <kenthesage@gmail.com>
2013-04-18 19:19:52 -0400

    Refactor LexError tests

--- test/lex_helper.py
+++ test/lex_helper.py
@@ -39,12 +39,12 @@ class LexerTestCase(unittest.TestCase):
                 )
             )
 
-    def assertLexError(self, string, error_count=1):
+    def assertLexError(self, string):
         with warnings.catch_warnings(record=True) as w:
             warnings.simplefilter('always')
 
             lexer.input(string)
             # We need to lex the whole string to get any errors
             [tok for tok in lexer]
-            if len([issubclass(e.category, LexError) for e in w]) != error_count:
+            if len([issubclass(e.category, LexError) for e in w]) == 0:
                 raise AssertionError('Expected LexError not raised!')
