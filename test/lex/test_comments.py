e8f51246a58f4925b7a39034c08af369bd8fb685
belai <blencho04@gmail.com>
2013-04-19 17:24:35 -0400

    added test_comments.py

--- test/lex/test_comments.py
+++ test/lex/test_comments.py
@@ -5,21 +5,21 @@ from test.lex_helper import LexerTestCase
 class TestComments(LexerTestCase):
 	def test_basic_comment(self):
 		self.assertTokenTypes(
-		'# Hi there guys!',
+			'# Hi there guys!',
 		)
 	def test_code_comment(self):
 		self.assertTokenTypes(
-		'int c # Hi there guys!',
-		('int', 'INT'),
-		('c', 'NAME'),
+			'int c # Hi there guys!',
+			('int', 'INT'),
+			('c', 'NAME'),
 		)
 	def test_code_comment(self):
 		self.assertTokenTypes(
-		r"""int c # Hi there line 1
-		# Hi there line 2
-		""",
-		('int', 'INT'),
-		('c', 'NAME'),
-		('\n', 'NEWLINE'),
-		('\n', 'NEWLINE'),
+			r"""int c # Hi there line 1
+			# Hi there line 2
+			""",
+			('int', 'INT'),
+			('c', 'NAME'),
+			('\n', 'NEWLINE'),
+			('\n', 'NEWLINE'),
 		)
