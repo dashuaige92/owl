eb3ec1b66d570db6f47260455edccf6fe5218426
Kenneth Cheng <kenthesage@gmail.com>
2013-05-11 17:39:50 -0400

    Fix binary_mod_three

--- owl/lex.py
+++ owl/lex.py
@@ -117,7 +117,7 @@ def t_NEWLINE(t):
     return t
 
 def t_error(t):
-    warnings.warn("Illegal character '%s'"  %t.value[0], LexError)
+    warnings.warn("Illegal character '%s'" % t.value[0], LexError)
     t.lexer.skip(1)
 
 lexer = lex.lex()
