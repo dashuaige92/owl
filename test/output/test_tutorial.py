f7e19d1117a66664eee240d55b61ed5232bf7ca6
martinowns <martinowns@gmail.com>
2013-05-11 20:09:07 -0400

    tests from tutorial

---
+++ test/output/test_tutorial.py
@@ -0,0 +1,31 @@
+import unittest
+import textwrap
+
+from test.output_helper import OutputTestCase
+
+class TestTutorial(OutputTestCase):
+
+    def test_tutorial_variables_and_functions(self):
+        owl = 'examples/tutorial_variables_functions.owl'
+        expected_output = "Hello world\nHello world\nHello world\n7\n"
+        self.assertOutput(owl, expected_output)
+
+    def test_tutorial_lists(self):
+        owl = 'examples/tutorial_lists.owl'
+        expected_output = "cat\n"
+        self.assertOutput(owl, expected_output)
+
+    def test_tutorial_if_statements(self):
+        owl = 'examples/tutorial_if_statements.owl'
+        expected_output = "False\n"
+        self.assertOutput(owl, expected_output)
+
+    def test_tutorial_loops(self):
+        owl = 'examples/tutorial_loops.owl'
+        expected_output = "0\n1\n2\n3\n4\n5\n0\n1\n2\n3\n4\n5\n"
+        self.assertOutput(owl, expected_output)
+
+    def test_tutorial_machine_hello_world(self):
+        owl = 'examples/tutorial_machine_hello_world.owl'
+        expected_output = "Hello world\nHello world\n"
+        self.assertOutput(owl, expected_output)
