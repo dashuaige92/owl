60355d9f261e967520ac5edef5d480483e355d45
Kenneth Cheng <kenthesage@gmail.com>
2013-05-12 17:08:28 -0400

    Make run.py not work if errors exist, Add Melanie's file

--- run.py
+++ run.py
@@ -3,9 +3,20 @@ Usage: python run.py [owl-source-file]
 """
 
 import sys
+import warnings
 
 from owl import transform
 
 if __name__ == '__main__':
-    tree = transform.build_tree(sys.argv)
-    exec compile(tree, '<string>', mode='exec')
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+
+        tree = transform.build_tree(sys.argv)
+
+        if any(issubclass(e.category, Warning) for e in w):
+            print 'Errors found in Owl source!'
+            for e in w:
+                print e.message
+
+        else:
+            exec compile(tree, '<string>', mode='exec')
